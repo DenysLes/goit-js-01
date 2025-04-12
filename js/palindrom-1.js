760631180b0fc4e71dd5d9b6fcbd6e9f9fe15a2c
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-04-12 17:23:47 +0300

    done fuction palindrom

--- js/palindrom-1.js
+++ js/palindrom-1.js
@@ -1,8 +1,23 @@
-const mamo = 'Перемінна "mamo" в script-1';
+let arr = 12213;
+let arr1 = arr.toString().split('');
+console.log(arr1);
 
-console.log(mamo);
+function palindrom(number1, number2) {
+  for (let i = 0; i < arr1.length; i++) {
+    let el = arr1[i];
+    // console.log(el);
 
-const bamo = 'Перемінна "bamo" в script-1';
+    for (let k = arr1.length - 1; k >= 0; k--) {
+      let el1 = arr1[k];
+      console.log(el1);
 
-console.log(bamo);
+      if (el === el1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
 
+console.log(palindrom(arr1, arr1));
